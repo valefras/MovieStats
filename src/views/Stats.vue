2e34ad5634138ef5782437cfb165b2891a4b38bf
valefras <48183554+valefras@users.noreply.github.com>
2020-05-12 20:52:36 +0200

    tweak

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>My Stats</h1>
         <h3>{{ filmdata.length }} films</h3>
-        <h3>{{ lang.length }} languages</h3>
+        <!--     <h3>{{ lang.length }} languages</h3> -->
         <bar :filmdata="filmdata" />
         <bar1 :filmdata="filmdata" />
     </div>
@@ -17,11 +17,11 @@ export default {
         filmdata: Array,
     },
     components: { bar, bar1 },
-    date() {
+    /*   date() {
         return {
             lang: [],
         }
-    },
+    },*/
     /* created() {
         for (var i = 0; i < this.filmdata.length; i++) {
             if (this.lang.includes(this.filmdata[i].language)) {
