d0448fce18a5e20ef1235ce8d95c2b85135e6532
valefras <48183554+valefras@users.noreply.github.com>
2020-09-08 16:55:06 +0200

    Update create_session.vue

--- src/components/create_session.vue
+++ src/components/create_session.vue
@@ -40,8 +40,8 @@ export default {
                     window.location.href =
                         'https://www.themoviedb.org/authenticate/' +
                         this.requestToken +
-                        '?redirect_to=http://localhost:8080/#/'
-                    //https://moviestats-2ad27.web.app
+                        '?redirect_to=https://moviestats-2ad27.web.app'
+                    //http://localhost:8080/#/
                 })
                 .catch(function(error) {
                     console.log(error)
